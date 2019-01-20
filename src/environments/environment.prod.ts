62f678f64819b9fa67399ec45f4f8e96c43bc02b
OmarSherif96 <46057697+OmarSherif96@users.noreply.github.com>
2019-01-20 09:18:24 +0200

    Add files via upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  masterFirebaseConfig: {
+    apiKey: "AIzaSyBCTkvNgEFNsAwM3D7Sujc5IWjI11iis_U",
+      authDomain: "register-d1780.firebaseapp.com",
+      databaseURL: "https://register-d1780.firebaseio.com",
+      projectId: "register-d1780",
+      storageBucket: "register-d1780.appspot.com",
+      messagingSenderId: "743884932250"
+    }
 };
